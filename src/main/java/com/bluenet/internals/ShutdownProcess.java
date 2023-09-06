3fc16087536ad6c1f349aae61eaab1638b42fbb1
TntTastisch <tnttastisch@gmail.com>
2023-09-06 19:40:45 +0200

    update shutdown prrocessor

--- src/main/java/com/bluenet/internals/ShutdownProcess.java
+++ src/main/java/com/bluenet/internals/ShutdownProcess.java
@@ -1,10 +1,14 @@
 package com.bluenet.internals;
 
+import ch.qos.logback.classic.LoggerContext;
 import com.bluenet.logger.Zip;
 import com.bluenet.main.DiscordBot;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ShutdownProcess {
 
@@ -18,7 +22,10 @@ public class ShutdownProcess {
         logger.info("System is shutting down...");
         try {
             DiscordBot.getInstance().getConsoleThread().interrupt();
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+            context.getLogger(logger.getName()).getLoggerContext().stop();
             Zip.zipOldLogs();
+            Files.delete(Paths.get("logs/latest.log"));
         } catch (IOException e) {
             logger.error("An error occurred while zipping old log files: ", e);
         }
